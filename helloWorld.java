913e749fb808b53fc84ac0123bf02e7b353c1b2a
Afsar Hussain <afsar.hussain@sap.com>
2023-03-10 21:34:56 +0530

    Fourth Commit

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,7 @@
 public class helloWorld {
     public static void main(String[] args) {
+        String name1 = "AFSAR HUSSAIN";
         System.out.println("Hello world!");
-        System.out.println("Hello Afsar Hussain");
+        System.out.println(name1);
     }
 }
